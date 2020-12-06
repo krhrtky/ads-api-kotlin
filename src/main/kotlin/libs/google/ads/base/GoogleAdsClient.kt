f50fe7c7733654f999e59d4fe8799ba2557284e9
Takuya Kurihara <takuya.kurihara00@gmail.com>
2020-12-06 23:39:03 +0900

    Add AdsClient param.

--- src/main/kotlin/libs/google/ads/base/GoogleAdsClient.kt
+++ src/main/kotlin/libs/google/ads/base/GoogleAdsClient.kt
@@ -8,9 +8,13 @@ class GoogleAdsClient {
         private val developerToken = System.getenv("GOOGLE_ADS_DEVELOPER_TOKEN") ?: throw
         throw IllegalPropertyException("Fail create Google Ads Client. Developer token is blank.")
 
+        private val loginCustomerId = System.getenv("GOOGLE_ADS_LOGIN_CUSTOMER_ID") ?: throw
+        throw IllegalPropertyException("Fail create Google Ads Client. Login Customer ID is blank.")
+
         val instance = GoogleAdsClient
             .newBuilder()
             .setDeveloperToken(developerToken)
+            .setLoginCustomerId(loginCustomerId.toLong())
             .setCredentials(EnvironmentVariablesProvider.create())
             .build() ?: throw Exception("Fail create Google Ads Client. Occur error.")
     }
